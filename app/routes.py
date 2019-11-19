cccb4c5ceda865390fe8eae43460d8e784de410d
wkwii97 <wwatson@ithaca.edu>
2019-11-19 14:06:47 -0500

    Login before forms.py change

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-#from app.forms import LoginForm
+from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user
 
 @app.route('/')
@@ -22,17 +22,5 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
     form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user is None or not user.check_password(form.password.data):
-    #         flash('Invalid username or password')
-    #         return redirect(url_for('login'))
-    #     login_user(user, remember=form.remember_me.data)
-    #     next_page = request.args.get('next')
-    #     if not next_page or url_parse(next_page).netloc != '':
-    #         next_page = url_for('home')
-    #     return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
