d40e91f444739215d069f2fb34acd86bff827943
ruppatel <ruppatel11599@gmail.com>
2019-11-19 13:51:52 -0500

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
