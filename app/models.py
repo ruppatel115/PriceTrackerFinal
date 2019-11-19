5ad9293c58e232f77fe588c6890181b5c13f8b1d
ruppatel <ruppatel11599@gmail.com>
2019-11-19 13:54:37 -0500

    first commit

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    emailid = db.Column(db.Integer(), index=True, unique=True)
     password_hash = db.Column(db.String(128))
