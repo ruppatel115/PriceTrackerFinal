86d353bb0091902e32b75f66c6329700a9b77db0
E-3P0 <elias@jwplatt.com>
2019-12-02 18:34:31 -0500

    Fixed merge conflict in routes and added some things for editing profile

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,18 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
--- app/routes.py
+++ app/routes.py
@@ -87,10 +87,25 @@ def item():
 @app.route('/profile')
 @login_required
 def profile():
-    # currentuser = User.query.filter_by(username={{current_user.name}}).first()
-    # useritems = Item.query.filter_by(userId=currentuser.id).all()
+    currentuser = User.query.filter_by(username={{current_user.username}}).first()
+    useritems = Item.query.filter_by(userId=currentuser.id).all()
 
-    return render_template('profile.html',title='Profile')#,items=useritems)
+    return render_template('profile.html',title='Profile', items=useritems)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('edit_profile.html', title='Edit Profile',form=form)
 
 
 @app.route('/data')
